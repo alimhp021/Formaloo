49fa06c0fc691333cab99ab790896f36ad2d2ba5
Reyhaneh <reyhanehkhoshghadam@gmail.com>
2024-07-30 16:35:43 +0330

    Revert "init front-end-remove extra files"
    This reverts commit 929f0715143143cfd4f236200eb7e0edce551236.

--- front-end/src/App.tsx
+++ front-end/src/App.tsx
@@ -1,13 +1,10 @@
-import React from "react";
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
 import "./App.css";
-import { SidePanel } from "./Components/SidePanel";
 
 function App() {
-  return (
-    <div className="App">
-      <SidePanel></SidePanel>
-    </div>
-  );
+  return <></>;
 }
 
 export default App;
