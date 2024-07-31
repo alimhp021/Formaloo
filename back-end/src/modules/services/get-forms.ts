412bd8294c67b103463aa80e9adae887fb7d82cf
Alireza Izadi <alirezaizadi199912@gmail.com>
2024-07-31 15:38:47 +0330

    Add getForm method to FormService

--- back-end/src/modules/services/get-forms.ts
+++ back-end/src/modules/services/get-forms.ts
@@ -2,4 +2,8 @@ import { FormRepository } from "../repository/form.repository";
 
 export class FormService {
     constructor(private formRepository: FormRepository) {}
+
+    getForms = async () => {
+        return await this.formRepository.getForms();
+    }
 }
