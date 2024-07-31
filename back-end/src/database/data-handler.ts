4fa98f820d572c19e311b9a1c92937b7bd0a57ae
Alireza Izadi <alirezaizadi199912@gmail.com>
2024-07-31 14:51:27 +0330

    Create config.ts and build FormRepository and FormService

--- back-end/src/database/data-handler.ts
+++ back-end/src/database/data-handler.ts
@@ -3,11 +3,11 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
-export const makeDataHandler = async () => {
+export const makeDataHandler = () => {
     if (!process.env.DB_CONNECTION_STRING) {
         throw new Error("Connection string is not set in environment variables.");
     }
-    await mongoose.connect(process.env.DB_CONNECTION_STRING);
+    mongoose.connect(process.env.DB_CONNECTION_STRING);
     console.log("Connected to Mongo DB");
     return mongoose;
 }
