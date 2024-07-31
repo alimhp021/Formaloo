8d431cc0dfa4552a169651b4e5ac40b88109f6c7
Alireza Izadi <alirezaizadi199912@gmail.com>
2024-07-31 16:04:30 +0330

    Add getFormList route

--- back-end/src/modules/routes/form.route.ts
+++ back-end/src/modules/routes/form.route.ts
@@ -1,3 +1,13 @@
 import {Router} from "express";
+import { formService } from "../../config";
 
 export const formRouter = Router();
+
+formRouter.get("/getFormList", (req, res) => {
+    if (!req.headers.authorization) {
+        res.status(401).send({message: "You are not logged in"});
+        return;
+    }
+    const forms = formService.getForms(req.headers.authorization);
+    res.send(forms);
+});
