0de3cfcbc8c81b2849aef65e461ced065bfe2e8b
Alireza Izadi <alirezaizadi199912@gmail.com>
2024-07-31 12:44:26 +0330

    Add routing middleware to app

--- back-end/src/startup.ts
+++ back-end/src/startup.ts
@@ -1,12 +1,13 @@
 import express from "express";
+import { formRouter } from "./modules/routes/form.route";
 
 export const makeApp = () => {
     const app = express();
 
     app.use(express.json());
 
-    // routes here.
-    
+    app.use(formRouter);
+
     app.use((req, res) => {
         res.status(404).send({message: "Page not found"});
     });
