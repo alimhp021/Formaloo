567ee20df05219c48c6f80a47576cb3f04a05454
Alireza Izadi <alirezaizadi199912@gmail.com>
2024-08-01 13:49:14 +0330

    Change makeDataHandler so that it logs connection success after resolving promise

--- back-end/src/database/data-handler.ts
+++ back-end/src/database/data-handler.ts
@@ -7,7 +7,9 @@ export const makeDataHandler = () => {
     if (!process.env.DB_CONNECTION_STRING) {
         throw new Error("Connection string is not set in environment variables.");
     }
-    mongoose.connect(process.env.DB_CONNECTION_STRING);
-    console.log("Connected to Mongo DB");
+    mongoose.connect(process.env.DB_CONNECTION_STRING)
+    .then(() => {
+        console.log("Connected to Mongo DB");
+    });
     return mongoose;
 }
