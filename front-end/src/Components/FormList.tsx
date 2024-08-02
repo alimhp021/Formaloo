d7c9534cfa5bc66becaed5bebc6d408b2bf96382
Reyhaneh <reyhanehkhoshghadam@gmail.com>
2024-08-02 14:20:16 +0330

    updates formlist

--- front-end/src/Components/FormList.tsx
+++ front-end/src/Components/FormList.tsx
@@ -1,41 +1,35 @@
-import React, { useReducer, useState } from "react";
-import { useForm } from "react-hook-form";
+import React, { useState } from "react";
 import "../App.css";
 
 interface FormType {
   title: string;
-  dateCreated: Date;
-  dateModified: Date;
+  dateCreated: string;
+  dateModified: string;
   status: "published" | "unpublished";
 }
 
 const FormList = () => {
-  const { register, handleSubmit } = useForm<FormType[]>({
-    defaultValues: [
-      {
-        title: "test",
-        dateCreated: new Date(Date.now()),
-        dateModified: new Date(Date.now()),
-        status: "unpublished",
-      },
-    ],
-  });
-  const [forms, setForms] = useState<FormType[]>([
-    {
-      title: "test",
-      dateCreated: new Date(Date.now()),
-      dateModified: new Date(Date.now()),
-      status: "unpublished",
-    },
-  ]);
-  return forms.map((form) => {
+  const [forms, setForms] = useState<FormType[]>([]);
+
+  //api call to setForms
+
+  return forms.map((form, index) => {
     return (
       <div className="Form">
         <span>{form.title}</span>
         <span>Date Created: {`${form.dateCreated}`} </span>
         <span>Date Modified: {`${form.dateModified}`}</span>
         <span>Status: {form.status}</span>
-        <button>Delete</button>
+        <button
+          onClick={() => {
+            setForms((forms) => {
+              return forms.slice(0, index).concat(forms.slice(index + 1));
+            });
+            // api call new form list
+          }}
+        >
+          Delete
+        </button>
         <button>Edit</button>
         <button>View Results</button>
       </div>
